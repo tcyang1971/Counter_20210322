d0ea1f67c932be563d9293b6b7b77f3f24b774a4
tcyang <tcyang@gm.pu.edu.tw>
2021-03-29 11:45:24 +0800

    震動

--- app/src/main/java/tw/edu/pu/csim/tcyang/counter_20210322/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/counter_20210322/MainActivity.kt
@@ -1,13 +1,17 @@
 package tw.edu.pu.csim.tcyang.counter_20210322
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.view.MotionEvent
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchListener {
 
     var counter:Int = 0
 
@@ -35,18 +39,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         })
 
 
-        txvvb.setOnTouchListener(object:View.OnTouchListener{
-            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                //txvvb.text="觸控測試"
-                if (event?.action == MotionEvent.ACTION_DOWN){
-                    txvvb.text="手指壓下"
-                }
-                else if (event?.action == MotionEvent.ACTION_UP){
-                    txvvb.text="手指彈開"
-                }
-                return true
-            }
-        })
+        txvvb.setOnTouchListener(this)
     }
 
 
@@ -80,6 +73,26 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
          */
         txv.text = counter.toString()
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        //txvvb.text="觸控測試"
+        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (event?.action == MotionEvent.ACTION_DOWN){
+            txvvb.text="手指壓下"
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                //Build.VERSION.SDK_INT >= 26, New vibrate method for API Level 26 or higher
+                vibrator.vibrate(VibrationEffect.createOneShot(5000, VibrationEffect.DEFAULT_AMPLITUDE))
+            } else {
+                vibrator.vibrate(5000)
+            }
+
+        }
+        else if (event?.action == MotionEvent.ACTION_UP){
+            txvvb.text="手指彈開"
+            vibrator.cancel()
+        }
+        return true
     }
 
 
